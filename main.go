5929f91bc76881070e70ada8cbd8b538f911320e
ginozza <jsimancas@unimagdalena.edu.co>
2024-07-12 18:03:27 -0500

    Add seeds handler

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 )
 
+
+func seedAccount(store Storage, fname, lname, pw string) *Account{
+	acc, err := NewAccount(fname, lname, pw)
+    if err!= nil {
+        log.Fatalf("error creating account: %v", err)
+    }
+
+    if err := store.CreateAccount(acc); err != nil {
+		log.Fatalf("error saving account: %v", err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage){
+	seedAccount(s, "Antonio", "Villa", "contra88")
+}
+
 func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+	
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -13,6 +38,11 @@ func main() {
 	if err := store.Init(); err != nil {
 		log.Fatal(err)
 	}
+
+	if *seed {
+		fmt.Println("seeding the database")
+        seedAccounts(store)
+    }
 
 	server := NewAPIserver(":3000", store)
 	server.Run()
